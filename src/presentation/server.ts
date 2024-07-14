11279a84da3ba204e66f4ae1356fa8516b958c7b
Sebastian <sebastianvalbuena25@gmail.com>
2024-07-14 08:28:28 -0500

    Empezando el proyecto

---
+++ src/presentation/server.ts
@@ -0,0 +1,20 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service"
+
+export class Server {
+    static start() {
+        console.log('Server started..')
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
+                ).execute(url);
+                // new CheckService().execute('http://localhost:3000/posts');
+            });
+    }
+} 
